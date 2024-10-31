3e0db96fbd24bf7c936f4d3b75467f3e619cb96d
abdullah-lulu <abood110120130@gmail.com>
2024-10-31 05:13:37 +0200

    first commit

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    string[] a ={"Cat","Dog","Car","Pizza","Hat","Fish","Tree","Monkey","Ball","Bird"};   
+        int b = (9);
+        string score = "";
+
+    
+        while ( b > 2) 
+        {
+           score = score + a [Random.Range(0,9)]+" ";
+            b--;
+        }
+        Debug.Log(score);
+
+    }
+
+ }
