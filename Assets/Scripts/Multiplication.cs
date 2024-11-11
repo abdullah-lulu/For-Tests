12328ee033d6c62cc1a16aafb70f47c91d763e3b
abdullah-lulu <abood110120130@gmail.com>
2024-11-11 03:10:37 +0200

    update jo

---
+++ Assets/Scripts/Multiplication.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiplication : MonoBehaviour
+{
+    void Start()
+    {
+        int num1;
+        int num2;
+
+        for (int x = 1; x <= 10; x++)
+        {
+            num2 = 5;
+            int s = Multiply(x,num2);
+            Debug.Log(x + " x "+ num2 + " = " + s);
+
+
+
+
+        }
+    
+    }
+
+
+    int Multiply(int x, int num2)
+    {
+        int sum = x * num2;
+        return sum;
+    }
+}
